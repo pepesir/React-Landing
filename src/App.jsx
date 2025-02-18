c85c9a48bba6c794a56a77df6235811c2335842c
pepesir <mohammedroshan.p.r.2345@gmail.com>
2025-02-18 15:06:58 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import React from 'react'
-import Header from './components/dashboard/Header'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import React from "react";
+import Header from "./components/dashboard/Header";
 function App() {
   return (
     <>
-    <Header/>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
